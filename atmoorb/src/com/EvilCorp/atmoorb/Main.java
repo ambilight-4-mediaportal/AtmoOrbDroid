abb562704adcbe2ea19a1b626d7461f013cd51d1
Rick <RickDB@gmail.com>
2015-09-23 18:33:25 +0200

    Added new setting to specify led count. Reformatted code and cleaned out old references.

--- atmoorb/src/com/EvilCorp/atmoorb/Main.java
+++ atmoorb/src/com/EvilCorp/atmoorb/Main.java
@@ -14,6 +14,7 @@ import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
@@ -31,219 +32,227 @@ import java.util.List;
 
 public class Main extends Activity {
 
-  // Default ColorMixer settings
-  private static final int COLOR_REQUEST=1337;
-  private TextView color=null;
-  private ColorMixer mixer=null;
+    // Default ColorMixer settings
+    private static final int COLOR_REQUEST = 1337;
+    private TextView color = null;
+    private ColorMixer mixer = null;
 
-  // Log settings
-  private static final String TAG = ColorMixer.class.getSimpleName();
+    // Log settings
+    private static final String TAG = ColorMixer.class.getSimpleName();
 
-  // Orb Settings
-  private EditText editTextOrbID;
+    // Orb Settings
+    private EditText editTextOrbID;
+    private EditText editTextOrbLedCount;
 
-  // Multicast settings
-  MulticastSocket m_socket;
-  InetAddress mMulticastAddress;
+    // Multicast settings
+    MulticastSocket m_socket;
+    InetAddress mMulticastAddress;
 
-  // Current Orb command information
-  private static int cRed;
-  private static int cGreen;
-  private static int cBlue;
-  private static int cCommandOption;
-  private static String cOrbIDs;
+    // Current Orb command information
+    private static int cRed;
+    private static int cGreen;
+    private static int cBlue;
+    private static int cCommandOption;
+    private static String cOrbIDs;
+    private static String cOrbLedCount;
 
-  @Override
-  public void onCreate(Bundle icicle) {
+    @Override
+    public void onCreate(Bundle icicle) {
 
     /* Turn off multicast filter */
-    WifiManager wm = (WifiManager)getSystemService(Context.WIFI_SERVICE);
-    WifiManager.MulticastLock multicastLock = wm.createMulticastLock("debuginfo");
-    multicastLock.acquire();
+        WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiManager.MulticastLock multicastLock = wm.createMulticastLock("debuginfo");
+        multicastLock.acquire();
 
-    super.onCreate(icicle);
-    setContentView(R.layout.main);
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
 
-    color=(TextView)findViewById(R.id.color);
+        color = (TextView) findViewById(R.id.color);
 
-    mixer=(ColorMixer)findViewById(R.id.mixer);
-    mixer.setOnColorChangedListener(onColorChange);
+        mixer = (ColorMixer) findViewById(R.id.mixer);
+        mixer.setOnColorChangedListener(onColorChange);
 
-    editTextOrbID = (EditText) findViewById(R.id.tbOrbId);
+        editTextOrbID = (EditText) findViewById(R.id.tbOrbId);
+        editTextOrbLedCount = (EditText) findViewById(R.id.tbOrbLedCount);
 
-    MultiCastInstance();
-    LoadSettings();
+        MultiCastInstance();
+        LoadSettings();
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        editTextOrbID.addTextChangedListener(new TextWatcher() {
 
-    editTextOrbID.addTextChangedListener(new TextWatcher() {
-
-      public void afterTextChanged(Editable s) {
-        SaveSettings();
-      }
-
-      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-      }
-
-      public void onTextChanged(CharSequence s, int start, int before, int count) {
-        SaveSettings();
-      }
-    });
-  }
-
-  public void MultiCastInstance()
-  {
-    try {
-      mMulticastAddress = InetAddress.getByName("239.15.18.2");
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    }
-
-    try {
-      m_socket = new MulticastSocket(49692);
-      m_socket.setSendBufferSize(50000);
-      m_socket.setReceiveBufferSize(50000);
-
-      m_socket.joinGroup(mMulticastAddress);
-      m_socket.setLoopbackMode(true);
-      m_socket.setTimeToLive(16);
-    }
-      catch (IOException e) {
-        e.printStackTrace();
-      }
-  }
-
-  public class SettingsActivity extends PreferenceActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      addPreferencesFromResource(R.xml.preferences);
-    }
-  }
-
-  @Override
-  public void onActivityResult(int requestCode, int resultCode,
-                               Intent result) {
-    if (requestCode==COLOR_REQUEST && resultCode==RESULT_OK) {
-      mixer.setColor(result.getIntExtra(ColorMixerActivity.COLOR,
-              mixer.getColor()));
-    }
-    else {
-      super.onActivityResult(requestCode, resultCode, result);
-    }
-  }
-
-  private void LoadSettings()
-  {
-    editTextOrbID.setText(PreferenceManager.getDefaultSharedPreferences(this).getString("OrbID", ""));
-
-    int argb = PreferenceManager.getDefaultSharedPreferences(this).getInt("OrbColors", 0);
-    mixer.setColor(argb);
-  }
-
-  private void SaveSettings()
-  {
-    int argb = mixer.getColor();
-
-    SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
-    editor.putString("OrbID", editTextOrbID.getText().toString());
-    editor.putInt("OrbColors", argb);
-    editor.commit();
-  }
-
-  private ColorMixer.OnColorChangedListener onColorChange=
-          new ColorMixer.OnColorChangedListener() {
-            public void onColorChange(int argb) {
-
-              /// On color change send commands to Orb
-              Log.d(TAG, String.valueOf(Color.red(argb)));
-              Log.d(TAG, String.valueOf(Color.green(argb)));
-              Log.d(TAG, String.valueOf(Color.blue(argb)));
-
-              int red = Color.red(argb);
-              int green = Color.green(argb);
-              int blue = Color.blue(argb);
-
-              String colorText = "R: " + red + " / " + "G: " + green + " / " +"B: " + blue;
-              color.setText(colorText);
-              String OrbID = String.valueOf(editTextOrbID.getText());
-              SaveSettings();
-              setColor(red,green,blue, OrbID, 2);
+            public void afterTextChanged(Editable s) {
+                SaveSettings();
             }
-          };
 
-  private void setColor(int red, int green, int blue, String orbID, int commandOption)
-  {
-    cRed = red;
-    cGreen = green;
-    cBlue = blue;
-    cOrbIDs = orbID;
-    cCommandOption = commandOption;
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-    new SendMultiCastData().execute("");
-  }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                SaveSettings();
+            }
+        });
+    }
 
-  private class SendMultiCastData extends AsyncTask<String, Void, String> {
-    protected String doInBackground(String... params) {
-      {
+    public void MultiCastInstance() {
         try {
-
-          List<String> Orbs = new ArrayList<>();
-          Orbs.add(cOrbIDs);
-
-          if (cOrbIDs.contains(",")) {
-            Orbs.clear();
-            String[] splitcOrbIDs = cOrbIDs.split(",");
-
-            for (String orb : splitcOrbIDs) {
-              Orbs.add(orb);
-            }
-          }
-
-          for (String orb : Orbs) {
-            byte commandCount = 24;
-            byte[] bytes = new byte[5 + commandCount * 3];
-
-            // Command identifier: C0FFEE
-            bytes[0] = (byte) 0xC0;
-            bytes[1] = (byte) 0xFF;
-            bytes[2] = (byte) 0xEE;
-            bytes[3] = (byte) cCommandOption;
-
-            // Orb ID
-            bytes[4] = Byte.parseByte(orb);
-
-            // RED / GREEN / BLUE
-            bytes[5] = (byte) cRed;
-            bytes[6] = (byte) cGreen;
-            bytes[7] = (byte) cBlue;
-
-            DatagramPacket dp = new DatagramPacket(bytes, bytes.length, mMulticastAddress, 49692);
-            try {
-              m_socket.send(dp);
-            } catch (Exception e) {
-              e.printStackTrace();
-            }
-            try {
-              Thread.sleep(1);
-            } catch (InterruptedException e) {
-              e.printStackTrace();
-            }
-          }
+            mMulticastAddress = InetAddress.getByName("239.15.18.2");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         }
-        catch (Exception e){}
-      }
-      return null;
+
+        try {
+            m_socket = new MulticastSocket(49692);
+            m_socket.setSendBufferSize(50000);
+            m_socket.setReceiveBufferSize(50000);
+
+            m_socket.joinGroup(mMulticastAddress);
+            m_socket.setLoopbackMode(true);
+            m_socket.setTimeToLive(16);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  private ColorMixer.OnColorChangedListener onDialogSet=
-          new ColorMixer.OnColorChangedListener() {
-            public void onColorChange(int argb) {
-              mixer.setColor(argb);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent result) {
+        if (requestCode == COLOR_REQUEST && resultCode == RESULT_OK) {
+            mixer.setColor(result.getIntExtra(ColorMixerActivity.COLOR,
+                    mixer.getColor()));
+        } else {
+            super.onActivityResult(requestCode, resultCode, result);
+        }
+    }
+
+    private void LoadSettings() {
+        String orbID = PreferenceManager.getDefaultSharedPreferences(this).getString("OrbID", "");
+        String ledCount = PreferenceManager.getDefaultSharedPreferences(this).getString("OrbLedCount", "");
+
+        if (orbID == "") {
+            editTextOrbID.setText("1");
+        } else {
+            editTextOrbID.setText(orbID);
+        }
+
+        if (ledCount == "") {
+            editTextOrbLedCount.setText("24");
+        } else {
+            editTextOrbLedCount.setText(ledCount);
+        }
+
+        int argb = PreferenceManager.getDefaultSharedPreferences(this).getInt("OrbColors", 0);
+        mixer.setColor(argb);
+    }
+
+    private void SaveSettings() {
+        int argb = mixer.getColor();
+
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putString("OrbID", editTextOrbID.getText().toString());
+        editor.putString("OrbLedCount", editTextOrbLedCount.getText().toString());
+        editor.putInt("OrbColors", argb);
+        editor.commit();
+    }
+
+    private ColorMixer.OnColorChangedListener onColorChange =
+            new ColorMixer.OnColorChangedListener() {
+                public void onColorChange(int argb) {
+
+                    /// On color change send commands to Orb
+                    Log.d(TAG, String.valueOf(Color.red(argb)));
+                    Log.d(TAG, String.valueOf(Color.green(argb)));
+                    Log.d(TAG, String.valueOf(Color.blue(argb)));
+
+                    int red = Color.red(argb);
+                    int green = Color.green(argb);
+                    int blue = Color.blue(argb);
+
+                    String colorText = "R: " + red + " / " + "G: " + green + " / " + "B: " + blue;
+                    color.setText(colorText);
+                    String OrbID = String.valueOf(editTextOrbID.getText());
+                    String OrbLedCount = String.valueOf(editTextOrbLedCount.getText());
+                    SaveSettings();
+                    setColor(red, green, blue, OrbID, OrbLedCount, 2);
+                }
+            };
+
+    private void setColor(int red, int green, int blue, String orbID, String orbLedCount, int commandOption) {
+        cRed = red;
+        cGreen = green;
+        cBlue = blue;
+        cOrbIDs = orbID;
+        cOrbLedCount = orbLedCount;
+        cCommandOption = commandOption;
+
+        new SendMultiCastData().execute("");
+    }
+
+    private class SendMultiCastData extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... params) {
+            {
+                try {
+
+                    List<String> Orbs = new ArrayList<String>();
+                    Orbs.add(cOrbIDs);
+
+                    if (cOrbIDs.contains(",")) {
+                        Orbs.clear();
+                        String[] splitcOrbIDs = cOrbIDs.split(",");
+
+                        for (String orb : splitcOrbIDs) {
+                            Orbs.add(orb);
+                        }
+                    }
+
+                    for (String orb : Orbs) {
+                        byte LedCount = Byte.parseByte(cOrbLedCount);
+                        byte[] bytes = new byte[5 + LedCount * 3];
+
+                        // Command identifier: C0FFEE
+                        bytes[0] = (byte) 0xC0;
+                        bytes[1] = (byte) 0xFF;
+                        bytes[2] = (byte) 0xEE;
+                        bytes[3] = (byte) cCommandOption;
+
+                        // Orb ID
+                        bytes[4] = Byte.parseByte(orb);
+
+                        // RED / GREEN / BLUE
+                        bytes[5] = (byte) cRed;
+                        bytes[6] = (byte) cGreen;
+                        bytes[7] = (byte) cBlue;
+
+                        DatagramPacket dp = new DatagramPacket(bytes, bytes.length, mMulticastAddress, 49692);
+                        try {
+                            m_socket.send(dp);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(1);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (Exception e) {
+                }
             }
-          };
+            return null;
+        }
+    }
 
-  public void btnTurnOffLights(View v) {
-    String OrbID = String.valueOf(editTextOrbID.getText());
-    setColor(0, 0, 0, OrbID, 1);
-  }
+    private ColorMixer.OnColorChangedListener onDialogSet =
+            new ColorMixer.OnColorChangedListener() {
+                public void onColorChange(int argb) {
+                    mixer.setColor(argb);
+                    editTextOrbID.clearFocus();
+                    editTextOrbLedCount.clearFocus();
+                }
+            };
+
+    public void btnTurnOffLights(View v) {
+        String OrbID = String.valueOf(editTextOrbID.getText());
+        String OrbLedCount = String.valueOf(editTextOrbLedCount.getText());
+        setColor(0, 0, 0, OrbID, OrbLedCount, 1);
+    }
 }
